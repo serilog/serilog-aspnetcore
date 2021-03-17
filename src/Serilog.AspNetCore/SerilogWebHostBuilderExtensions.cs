d9add52ef6c97a92ede1aeb86ae66a1376ab0dc4
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2021-03-18 08:55:37 +1000

    Explicitly qualify Serilog types in crefs

--- src/Serilog.AspNetCore/SerilogWebHostBuilderExtensions.cs
+++ src/Serilog.AspNetCore/SerilogWebHostBuilderExtensions.cs
@@ -16,7 +16,6 @@ using System;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
-using Serilog.Core;
 using Serilog.Extensions.Hosting;
 using Serilog.Extensions.Logging;
 
@@ -33,8 +32,8 @@ namespace Serilog
         /// <param name="builder">The web host builder to configure.</param>
         /// <param name="logger">The Serilog logger; if not supplied, the static <see cref="Serilog.Log"/> will be used.</param>
         /// <param name="dispose">When true, dispose <paramref name="logger"/> when the framework disposes the provider. If the
-        /// logger is not specified but <paramref name="dispose"/> is true, the <see cref="Log.CloseAndFlush()"/> method will be
-        /// called on the static <see cref="Log"/> class instead.</param>
+        /// logger is not specified but <paramref name="dispose"/> is true, the <see cref="Serilog.Log.CloseAndFlush()"/> method will be
+        /// called on the static <see cref="Serilog.Log"/> class instead.</param>
         /// <param name="providers">A <see cref="LoggerProviderCollection"/> registered in the Serilog pipeline using the
         /// <c>WriteTo.Providers()</c> configuration method, enabling other <see cref="ILoggerProvider"/>s to receive events. By
         /// default, only Serilog sinks will receive events.</param>
@@ -78,8 +77,8 @@ namespace Serilog
         /// The logger will be shut down when application services are disposed.
         /// </remarks>
         /// <param name="builder">The web host builder to configure.</param>
-        /// <param name="configureLogger">The delegate for configuring the <see cref="LoggerConfiguration" /> that will be used to construct a <see cref="Logger" />.</param>
-        /// <param name="preserveStaticLogger">Indicates whether to preserve the value of <see cref="Log.Logger"/>.</param>
+        /// <param name="configureLogger">The delegate for configuring the <see cref="LoggerConfiguration" /> that will be used to construct a <see cref="Serilog.Core.Logger" />.</param>
+        /// <param name="preserveStaticLogger">Indicates whether to preserve the value of <see cref="Serilog.Log.Logger"/>.</param>
         /// <param name="writeToProviders">By default, Serilog does not write events to <see cref="ILoggerProvider"/>s registered through
         /// the Microsoft.Extensions.Logging API. Normally, equivalent Serilog sinks are used in place of providers. Specify
         /// <c>true</c> to write events to all providers.</param>
