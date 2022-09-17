55d2469f09c86bbf142317b38c6995d407735be3
Simon Cropp <simon.cropp@gmail.com>
2022-09-17 21:20:26 +1000

    enable nullable

--- test/Serilog.AspNetCore.Tests/SerilogWebHostBuilderExtensionsTests.cs
+++ test/Serilog.AspNetCore.Tests/SerilogWebHostBuilderExtensionsTests.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
@@ -13,171 +10,173 @@ using Microsoft.AspNetCore.Http;
 using Serilog.Filters;
 using Serilog.AspNetCore.Tests.Support;
 using Serilog.Events;
-using System.Net.Http;
 
 // Newer frameworks provide IHostBuilder
 #pragma warning disable CS0618
 
-namespace Serilog.AspNetCore.Tests
+namespace Serilog.AspNetCore.Tests;
+
+public class SerilogWebHostBuilderExtensionsTests : IClassFixture<SerilogWebApplicationFactory>
 {
-    public class SerilogWebHostBuilderExtensionsTests : IClassFixture<SerilogWebApplicationFactory>
+    readonly SerilogWebApplicationFactory _web;
+
+    public SerilogWebHostBuilderExtensionsTests(SerilogWebApplicationFactory web)
     {
-        readonly SerilogWebApplicationFactory _web;
+        _web = web;
+    }
 
-        public SerilogWebHostBuilderExtensionsTests(SerilogWebApplicationFactory web)
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public async Task DisposeShouldBeHandled(bool dispose)
+    {
+        var logger = new DisposeTrackingLogger();
+        using (var web = Setup(logger, dispose))
         {
-            _web = web;
+            await web.CreateClient().GetAsync("/");
         }
 
-        [Theory]
-        [InlineData(true)]
-        [InlineData(false)]
-        public async Task DisposeShouldBeHandled(bool dispose)
-        {
-            var logger = new DisposeTrackingLogger();
-            using (var web = Setup(logger, dispose))
-            {
-                await web.CreateClient().GetAsync("/");
-            }
+        Assert.Equal(dispose, logger.IsDisposed);
+    }
 
-            Assert.Equal(dispose, logger.IsDisposed);
-        }
-
-        [Fact]
-        public async Task RequestLoggingMiddlewareShouldEnrich()
+    [Fact]
+    public async Task RequestLoggingMiddlewareShouldEnrich()
+    {
+        var (sink, web) = Setup(options =>
         {
-            var (sink, web) = Setup(options =>
+            options.EnrichDiagnosticContext += (diagnosticContext, _) =>
             {
-                options.EnrichDiagnosticContext += (diagnosticContext, httpContext) =>
+                diagnosticContext.Set("SomeInteger", 42);
+            };
+        });
+
+        await web.CreateClient().GetAsync("/resource");
+
+        Assert.NotEmpty(sink.Writes);
+
+        var completionEvent = sink.Writes.First(logEvent => Matching.FromSource<RequestLoggingMiddleware>()(logEvent));
+
+        Assert.Equal(42, completionEvent.Properties["SomeInteger"].LiteralValue());
+        Assert.Equal("string", completionEvent.Properties["SomeString"].LiteralValue());
+        Assert.Equal("/resource", completionEvent.Properties["RequestPath"].LiteralValue());
+        Assert.Equal(200, completionEvent.Properties["StatusCode"].LiteralValue());
+        Assert.Equal("GET", completionEvent.Properties["RequestMethod"].LiteralValue());
+        Assert.True(completionEvent.Properties.ContainsKey("Elapsed"));
+    }
+
+    [Fact]
+    public async Task RequestLoggingMiddlewareShouldEnrichWithCustomisedProperties()
+    {
+        var (sink, web) = Setup(options =>
+        {
+            options.MessageTemplate = "HTTP {RequestMethod} responded {Status} in {ElapsedMilliseconds:0.0000} ms";
+            options.GetMessageTemplateProperties = (ctx, _, elapsedMs, status) =>
+                new[]
                 {
-                    diagnosticContext.Set("SomeInteger", 42);
+                    new LogEventProperty("RequestMethod", new ScalarValue(ctx.Request.Method)),
+                    new LogEventProperty("Status", new ScalarValue(status)),
+                    new LogEventProperty("ElapsedMilliseconds", new ScalarValue(elapsedMs))
                 };
-            });
+        });
 
-            await web.CreateClient().GetAsync("/resource");
+        await web.CreateClient().GetAsync("/resource");
 
-            Assert.NotEmpty(sink.Writes);
+        Assert.NotEmpty(sink.Writes);
 
-            var completionEvent = sink.Writes.First(logEvent => Matching.FromSource<RequestLoggingMiddleware>()(logEvent));
+        var completionEvent = sink.Writes.First(logEvent => Matching.FromSource<RequestLoggingMiddleware>()(logEvent));
 
-            Assert.Equal(42, completionEvent.Properties["SomeInteger"].LiteralValue());
-            Assert.Equal("string", completionEvent.Properties["SomeString"].LiteralValue());
-            Assert.Equal("/resource", completionEvent.Properties["RequestPath"].LiteralValue());
-            Assert.Equal(200, completionEvent.Properties["StatusCode"].LiteralValue());
-            Assert.Equal("GET", completionEvent.Properties["RequestMethod"].LiteralValue());
-            Assert.True(completionEvent.Properties.ContainsKey("Elapsed"));
-        }
+        Assert.Equal("string", completionEvent.Properties["SomeString"].LiteralValue());
+        Assert.Equal(200, completionEvent.Properties["Status"].LiteralValue());
+        Assert.Equal("GET", completionEvent.Properties["RequestMethod"].LiteralValue());
+        Assert.True(completionEvent.Properties.ContainsKey("ElapsedMilliseconds"));
+        Assert.False(completionEvent.Properties.ContainsKey("Elapsed"));
+    }
 
-        [Fact]
-        public async Task RequestLoggingMiddlewareShouldEnrichWithCustomisedProperties()
+    [Fact]
+    public async Task RequestLoggingMiddlewareShouldEnrichWithCollectedExceptionIfNoUnhandledException()
+    {
+        var diagnosticContextException = new Exception("Exception set in diagnostic context");
+        var (sink, web) = Setup(options =>
         {
-            var (sink, web) = Setup(options =>
+            options.EnrichDiagnosticContext += (diagnosticContext, _) =>
             {
-                options.MessageTemplate = "HTTP {RequestMethod} responded {Status} in {ElapsedMilliseconds:0.0000} ms";
-                options.GetMessageTemplateProperties = (ctx, path, elapsedMs, status) =>
-                    new[]
-                    {
-                            new LogEventProperty("RequestMethod", new ScalarValue(ctx.Request.Method)),
-                            new LogEventProperty("Status", new ScalarValue(status)),
-                            new LogEventProperty("ElapsedMilliseconds", new ScalarValue(elapsedMs))
-                    };
-            });
+                diagnosticContext.SetException(diagnosticContextException);
+            };
+        });
 
-            await web.CreateClient().GetAsync("/resource");
+        await web.CreateClient().GetAsync("/resource");
 
-            Assert.NotEmpty(sink.Writes);
+        var completionEvent = sink.Writes.First(logEvent => Matching.FromSource<RequestLoggingMiddleware>()(logEvent));
 
-            var completionEvent = sink.Writes.First(logEvent => Matching.FromSource<RequestLoggingMiddleware>()(logEvent));
+        Assert.Same(diagnosticContextException, completionEvent.Exception);
+    }
 
-            Assert.Equal("string", completionEvent.Properties["SomeString"].LiteralValue());
-            Assert.Equal(200, completionEvent.Properties["Status"].LiteralValue());
-            Assert.Equal("GET", completionEvent.Properties["RequestMethod"].LiteralValue());
-            Assert.True(completionEvent.Properties.ContainsKey("ElapsedMilliseconds"));
-            Assert.False(completionEvent.Properties.ContainsKey("Elapsed"));
-        }
-
-        [Fact]
-        public async Task RequestLoggingMiddlewareShouldEnrichWithCollectedExceptionIfNoUnhandledException()
+    [Theory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public async Task RequestLoggingMiddlewareShouldEnrichWithUnhandledExceptionEvenIfExceptionIsSetInDiagnosticContext(bool setExceptionInDiagnosticContext)
+    {
+        var diagnosticContextException = new Exception("Exception set in diagnostic context");
+        var unhandledException = new Exception("Unhandled exception thrown in API action");
+        var (sink, web) = Setup(options =>
         {
-            var diagnosticContextException = new Exception("Exception set in diagnostic context");
-            var (sink, web) = Setup(options =>
+            options.EnrichDiagnosticContext += (diagnosticContext, _) =>
             {
-                options.EnrichDiagnosticContext += (diagnosticContext, _) =>
-                {
+                if (setExceptionInDiagnosticContext)
                     diagnosticContext.SetException(diagnosticContextException);
-                };
-            });
+            };
+        }, actionCallback: _ => throw unhandledException);
 
-            await web.CreateClient().GetAsync("/resource");
+        Func<Task> act = () => web.CreateClient().GetAsync("/resource");
 
-            var completionEvent = sink.Writes.First(logEvent => Matching.FromSource<RequestLoggingMiddleware>()(logEvent));
+        var thrownException = await Assert.ThrowsAsync<Exception>(act);
+        var completionEvent = sink.Writes.First(logEvent => Matching.FromSource<RequestLoggingMiddleware>()(logEvent));
+        Assert.Same(unhandledException, completionEvent.Exception);
+        Assert.Same(unhandledException, thrownException);
+    }
 
-            Assert.Same(diagnosticContextException, completionEvent.Exception);
-        }
-
-        [Theory]
-        [InlineData(false)]
-        [InlineData(true)]
-        public async Task RequestLoggingMiddlewareShouldEnrichWithUnhandledExceptionEvenIfExceptionIsSetInDiagnosticContext(bool setExceptionInDiagnosticContext)
-        {
-            var diagnosticContextException = new Exception("Exception set in diagnostic context");
-            var unhandledException = new Exception("Unhandled exception thrown in API action");
-            var (sink, web) = Setup(options =>
-            {
-                options.EnrichDiagnosticContext += (diagnosticContext, _) =>
+    WebApplicationFactory<TestStartup> Setup(
+        ILogger logger,
+        bool dispose,
+        Action<RequestLoggingOptions>? configureOptions = null,
+        Action<HttpContext>? actionCallback = null)
+    {
+        var web = _web.WithWebHostBuilder(
+            builder => builder
+                .ConfigureServices(sc => sc.Configure<RequestLoggingOptions>(options =>
                 {
-                    if (setExceptionInDiagnosticContext)
-                        diagnosticContext.SetException(diagnosticContextException);
-                };
-            }, actionCallback: _ => throw unhandledException);
-
-            Func<Task> act = () => web.CreateClient().GetAsync("/resource");
-
-            Exception thrownException = await Assert.ThrowsAsync<Exception>(act);
-            var completionEvent = sink.Writes.First(logEvent => Matching.FromSource<RequestLoggingMiddleware>()(logEvent));
-            Assert.Same(unhandledException, completionEvent.Exception);
-            Assert.Same(unhandledException, thrownException);
-        }
-
-        WebApplicationFactory<TestStartup> Setup(ILogger logger, bool dispose, Action<RequestLoggingOptions> configureOptions = null,
-            Action<HttpContext> actionCallback = null)
-        {
-            var web = _web.WithWebHostBuilder(
-                builder => builder
-                    .ConfigureServices(sc => sc.Configure<RequestLoggingOptions>(options =>
+                    options.Logger = logger;
+                    options.EnrichDiagnosticContext += (diagnosticContext, _) =>
                     {
-                        options.Logger = logger;
-                        options.EnrichDiagnosticContext += (diagnosticContext, httpContext) =>
-                        {
-                            diagnosticContext.Set("SomeString", "string");
-                        };
-                    }))
-                    .Configure(app =>
+                        diagnosticContext.Set("SomeString", "string");
+                    };
+                }))
+                .Configure(app =>
+                {
+                    app.UseSerilogRequestLogging(configureOptions);
+                    app.Run(ctx =>
                     {
-                        app.UseSerilogRequestLogging(configureOptions);
-                        app.Run(ctx =>
-                        {
-                            actionCallback?.Invoke(ctx);
-                            return Task.CompletedTask;
-                        }); // 200 OK
-                    })
-                    .UseSerilog(logger, dispose));
+                        actionCallback?.Invoke(ctx);
+                        return Task.CompletedTask;
+                    }); // 200 OK
+                })
+                .UseSerilog(logger, dispose));
 
-            return web;
-        }
+        return web;
+    }
 
-        (SerilogSink, WebApplicationFactory<TestStartup>) Setup(Action<RequestLoggingOptions> configureOptions = null,
-            Action<HttpContext> actionCallback = null)
-        {
-            var sink = new SerilogSink();
-            var logger = new LoggerConfiguration()
-                .Enrich.FromLogContext()
-                .WriteTo.Sink(sink)
-                .CreateLogger();
+    (SerilogSink, WebApplicationFactory<TestStartup>) Setup(
+        Action<RequestLoggingOptions>? configureOptions = null,
+        Action<HttpContext>? actionCallback = null)
+    {
+        var sink = new SerilogSink();
+        var logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .WriteTo.Sink(sink)
+            .CreateLogger();
 
-            var web = Setup(logger, true, configureOptions, actionCallback);
+        var web = Setup(logger, true, configureOptions, actionCallback);
 
-            return (sink, web);
-        }
+        return (sink, web);
     }
 }
